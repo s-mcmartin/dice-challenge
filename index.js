c37f2ed106da572bf93e9b0b3e9ddff9c1706c96
s-mcmartin <132164295+s-mcmartin@users.noreply.github.com>
2023-04-29 17:23:26 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImageSource);
+image2.setAttribute("src", randomImageSource2);
+
+var displayText = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2) {
+  displayText.innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  displayText.innerHTML = "Player 2 Wins!";
+} else if (randomNumber1 === randomNumber2) {
+  displayText.innerHTML = "Draw!";
+}
