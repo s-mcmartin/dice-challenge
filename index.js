bb9aa1e8479ec40ef386781aff936937defdb725
s-mcmartin <shannoncmcguire@outlook.com>
2023-04-29 18:56:44 -0400

    Add h4 element

--- index.js
+++ index.js
@@ -14,11 +14,22 @@ image1.setAttribute("src", randomImageSource);
 image2.setAttribute("src", randomImageSource2);
 
 var displayText = document.querySelector("h1");
+let displaySubText = document.querySelector('h4');
+
 
 if (randomNumber1 > randomNumber2) {
   displayText.innerHTML = "Player 1 Wins!";
+  
 } else if (randomNumber2 > randomNumber1) {
+  
   displayText.innerHTML = "Player 2 Wins!";
 } else if (randomNumber1 === randomNumber2) {
+  
   displayText.innerHTML = "Draw!";
 }
+
+if(displayText !== "Refresh Me") {
+  displaySubText.innerHTML = 'Refresh to play again.'
+}
+
+
